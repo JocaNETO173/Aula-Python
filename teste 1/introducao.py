d4111b750e29f2a007b4a0c0bde716ad97ea4e63
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2025-10-01 17:39:33 -0300

    Add files via upload

---
+++ teste 1/introducao.py
@@ -0,0 +1,16 @@
+idade = 18
+
+if idade >= 18:
+    print("Voce e maior de idade!")
+    print("Bloco 1")
+else:
+    print("Voce e menor de idade!")
+    print("Bloco 2")
+
+for i in range(0,10):
+    print(i)
+
+contador = 1
+while contador <= 10:
+    print(contador)
+    contador +=1
